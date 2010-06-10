6e16db9198bea739844cc580e999137bf0afe115
Artem.Votincev <devnull@localhost>
2010-06-10 14:03:16 +0400

    [svn r10] [add] asp.net example

---
+++ example/Asp.net/App_Code/GetPathHandler.cs
@@ -0,0 +1,147 @@
+using System.Drawing;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System;
+
+namespace App_Code
+{
+    /// <summary>
+    /// Summary description for GetVersionHistoryHandler.
+    /// </summary>
+    public class GetPathHandler : IHttpHandler
+    {
+        [DataContract]
+        private class BrowsePath
+        {
+            private string _Name;
+            private string _Date;
+            private string _Icon;
+            private int _Size;
+            private string _IsFolder;
+            private string _IsError;
+
+            [DataMember(Name = "name", Order = 0)]
+            public string Name
+            {
+                get { return _Name; }
+                set { _Name = value; }
+            }
+
+            [DataMember(Name = "date", Order = 1)]
+            public string Date
+            {
+                get { return _Date; }
+                set { _Date = value; }
+            }
+
+            [DataMember(Name = "icon", Order = 2)]
+            public string Icon
+            {
+                get { return _Icon; }
+                set { _Icon = value; }
+            }
+
+            [DataMember(Name = "size", Order = 3)]
+            public int Size
+            {
+                get { return _Size; }
+                set { _Size = value; }
+            }
+
+            [DataMember(Name = "isFolder", Order = 4)]
+            public string IsFolder
+            {
+                get { return _IsFolder; }
+                set { _IsFolder = value; }
+            }
+
+            [DataMember(Name = "isError", Order = 4)]
+            public string IsError
+            {
+                get { return _IsError; }
+                set { _IsError = value; }
+            }
+        }
+
+        private string BaseDirectory = "C:\\";
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            HttpRequest request = context.Request;
+            HttpServerUtility server = context.Server;
+
+            string path = request.QueryString["path"];
+
+            List<BrowsePath> item;
+            try
+            {
+                if (string.IsNullOrEmpty(BaseDirectory + path))
+                {
+                    item = (from device in Directory.GetLogicalDrives()
+                            select new BrowsePath { Name = device, IsFolder = "true" }).ToList();
+                }
+                else
+                {
+                    item = (from directory in Directory.GetDirectories(BaseDirectory + path)
+                            select new BrowsePath { Name = Path.GetFileName(directory) + "\\", IsFolder = "true" }).ToList();
+
+                    List<BrowsePath> files = (from file in Directory.GetFiles(BaseDirectory + path, "*.*")
+                                              select new BrowsePath { Name = file }).ToList();
+
+                    files.ForEach(bp =>
+                                      {
+                                          CreateIco(bp.Name, server.MapPath("~/img/browser/"));
+                                          bp.Name = Path.GetFileName(bp.Name);
+                                      });
+
+                    item.AddRange(files);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                item = new List<BrowsePath>();
+                item.Add(new BrowsePath { Name = ex.Message, IsError = "true" });
+            }
+
+            response.Clear();
+            response.ContentType = "text/plain";
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(item.GetType());
+            ser.WriteObject(HttpContext.Current.Response.OutputStream, item);
+            response.Flush();
+            response.Close();
+            response.End();
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        private static void CreateIco(string file, string iconDirect)
+        {
+            if (File.Exists(file))
+            {
+                string ext = Path.GetExtension(file);
+                if (ext.Length > 0)
+                    ext = ext.Substring(1);
+                else
+                    ext = "none";
+                string iconame = iconDirect + ext + ".png";
+                if (!File.Exists(iconame))
+                {
+                    using (FileStream fs = File.OpenWrite(iconame))
+                    {
+                        Icon.ExtractAssociatedIcon(file).Save(fs);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ example/Asp.net/Default.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>jq-serverBrowser example</title>
+    <script src="<%= ResolveUrl("~/")%>js/jquery.js" type="text/javascript"></script>
+    <script src="<%= ResolveUrl("~/")%>js/jquery-ui.js" type="text/javascript"></script>
+    <script src="<%= ResolveUrl("~/js/jquery.serverBrowser.js")%>" type="text/javascript"></script>
+    <link href="~/css/jquery-ui.css" rel="stylesheet" type="text/css" />
+    <link href="~/css/style.css" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript" language="javascript">
+        $(document).ready(function() {
+            $('#Button1').serverBrowser({
+                onSelect: function(path) {
+                    $('#TextBox1').val('c:\\' + path);
+                },
+                imageUrl: '<%=ResolveUrl("~/img/browser/") %>',
+                handlerUrl: '<%=ResolveUrl("~/GetPath.aspx") %>',
+                title: 'Browse1',
+                width: 300,
+                height: 500
+            });
+        });
+        
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <input id="TextBox1" style="width: 500px" />
+        <input id="Button1" type="button" class="ui-corner-all ui-state-default" value="Select" />
+        <br />
+    </div>
+    </form>
+</body>
+</html>
